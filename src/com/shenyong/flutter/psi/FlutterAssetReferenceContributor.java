a7d857ff6190cbdf69daaec4f1f711dcddfc2bfc
shenyong <shenyong812@hotmail.com>
2021-08-13 08:48:58 +0800

    fix: .9格式图片字符串支持。

--- src/com/shenyong/flutter/psi/FlutterAssetReferenceContributor.java
+++ src/com/shenyong/flutter/psi/FlutterAssetReferenceContributor.java
@@ -13,10 +13,6 @@ import org.jetbrains.annotations.NotNull;
  */
 public abstract class FlutterAssetReferenceContributor extends PsiReferenceContributor {
 
-    // eg: assets/images/doge.jpeg
-    public static final String ASSET_PATTERN = "^(\"|')?asset(s)?(/([-\\w]+|[1-9]\\.\\dx))*/[-\\w]+\\.(jp(e)?g|png|webp|bmp)(\"|')?$";
-    private static final StringPattern ASSET_STRING = StandardPatterns.string().matches(ASSET_PATTERN);
-
     public abstract Class<? extends PsiElement> provideAssetStringLiteralClass();
 
     public abstract ElementPattern<? extends PsiElement> provideElementPatterns();
